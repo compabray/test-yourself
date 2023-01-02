c9d918ec81a6804cd0c4c8750b14ece5c37fa44f
Brayan <brayanignacio.06.10@gmail.com>
2023-01-02 13:49:12 -0300

    feat: responsiveeeeee

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,14 +2,14 @@ import testList from '../pages/testList.json';
 import { NavLink, } from 'react-router-dom';
 
 function List(){
-    return ( <div className='flex m-auto w-8/12 justify-evenly mt-16'>
+    return ( <div className='flex m-auto w-full p-5 justify-evenly flex-wrap lg:flex-nowrap sm:mt-16 md:w-9/12'>
       {testList.map(({path, title, description, icon}) => {   
-            return (<NavLink to={path} className=' bg-zinc-800 p-4 w-64 rounded-md hover:bg-zinc-700'>
+            return (<NavLink to={path} className=' bg-zinc-800 my-2 p-4 w-full rounded-md hover:bg-zinc-700  sm:w-64' key={path}>
 
-                    <h3 className="text-2xl text-zinc-300 text-center">
+                    <h3 className="text-2xl text-zinc-300 text-center" key={title}>
                         {title} {icon}
                     </h3>
-                    <p className="text-zinc-400 text-base pt-2">
+                    <p className="text-zinc-400 text-base pt-2" key={description}>
                         {description}
                     </p>
                 </NavLink>);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import List from '../components/List';
 
 function Home () {
     return (
-        <div className="bg-zinc-900 h-screen">
-            <h1 className="p-8 font-semibold text-6xl text-purple-400 text-center">Test yourself!</h1>
-                <h3 className="w-7/12 p-3 text-zinc-400 m-auto text-lg">
+        <div className="">
+            <h1 className="p-8 font-semibold text-5xl text-purple-400 text-center sm:text-6xl">Test yourself!</h1>
+                <h3 className="w-full p-4 text-zinc-400 m-auto text-lg sm:w-10/12 md:w-7/12">
                     Try different tests and see what you are made of. Challenge your friends, have fun together and compare the results in the different questionaries!
                 </h3>
                 <List/>
