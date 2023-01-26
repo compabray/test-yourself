b67e8862675860fdf417f2724d6afda4b94ef66a
Brayan Rodríguez <68347498+compabray@users.noreply.github.com>
2023-01-26 14:50:49 -0300

    Deleting Undone questions.

--- src/components/testList.js
+++ src/components/testList.js
@@ -6,18 +6,5 @@ export const testList = [
         "title": "Rice Purity Test",
         "description": "A purity test created by rice university, if you have 100 you are very pure and if you get a 0 you are so unholy.",
         "icon":"😇 "
-    },
-    {   
-        "path":"gym",
-        "title": "Gym Test",
-        "description":"Check if in which level of the gym are you, and see if you are a real gym rat.",
-        "icon":"💪"
-    },
-    
-    {   
-        "path":"purity",
-        "title": "Purity test",
-        "description": "Check how pure you are with a few simple questions, and realize if you really are a child of god.",
-        "icon": "🙏" 
     }
 ];
